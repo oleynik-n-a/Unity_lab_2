00dfd490fc738aab5bdc898a2eae7966d1774766
Oleynik Nikita <naoleynik@edu.hse.ru>
2024-01-12 15:58:36 +0300

    added faceup

--- Assets/_Source/_Core/CardAsset.cs
+++ Assets/_Source/_Core/CardAsset.cs
@@ -1,3 +1,5 @@
+using System.Net.Mime;
+using System.Runtime.InteropServices.ComTypes;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,8 +7,12 @@ namespace _Source._Core
 {
     public class CardAsset
     {
-        private string _cardName;
-        private Color _cardColor;
-        private Image _cardImage;
+        public static readonly Sprite CardFront =
+            Resources.Load<Sprite>("from-the-game-textures-and-a-bit-of-my-bad-photoshop-skills-v0-5qgloz1kxz2a1");
+        public static readonly Sprite CardBack = Resources.Load<Sprite>("43a56ed9097f3f42bf5c691ba20b0d2d");
+        
+        public string CardName;
+        public Color CardColor;
+        public Image CardImage;
     }
 }
